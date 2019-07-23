18ceb13411176d261b2cef1c3dbb497b0970dab8
Megan Edmonds <meganr.edmonds@gmail.com>
2019-07-23 02:51:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -18,8 +18,8 @@ end
 def find_min_value(array)
   min_value = 0 
   array.length.times do |index|
-    if array[index] > max_value
-      max_value = array[index]
+    if array[index] < min_value
+      min_value = array[index]
     end
-  max_value
+  min_value
 end
