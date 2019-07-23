2f5132868decc08bee7bfdaa343e0d6421f332f8
Megan Edmonds <meganr.edmonds@gmail.com>
2019-07-23 03:29:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,4 +1,38 @@
 def find_element_index(array, value_to_find)
+  counter = 0
+  while array[counter] do
+    if array[counter] == value_to_find
+      return counter
+      counter += 1
+    end
+   counter
+end
+
+def find_max_value(array)
+  max_value = 0 
+  counter = 0
+  while array[counter] do
+    if array[counter] > max_value
+      max_value = array[counter]
+      counter += 1
+    end
+  max_value
+end
+
+def find_min_value(array)
+  min_value = 0 
+  index = 0
+  while array[index] do 
+    if array[index] < min_value
+      min_value = array[index]
+      index += 1
+    end
+  min_value
+end
+
+puts(find_element_index([2,3,4,5], 1))
+
+def find_element_index(array, value_to_find)
   return array.include?(value_to_find) ? array.index(value_to_find) : nil
 end
 
@@ -9,3 +43,4 @@ end
 def find_min_value(array)
  array.sort.first
 end
+
