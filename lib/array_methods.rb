c256d35ca337348e4aaf0520f40866b30ff12696
Megan Edmonds <meganr.edmonds@gmail.com>
2019-07-23 03:21:41 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,4 +1,14 @@
-
+def find_element_index(array, value_to_find)
+  counter = 0
+  while array[counter] do
+    if array[counter] == value_to_find
+      return counter
+      break
+      counter += 1
+    end
+  end
+   counter
+end
 
 def find_max_value(array)
   max_value = 0 
